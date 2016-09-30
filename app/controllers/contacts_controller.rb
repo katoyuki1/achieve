3387e0f339c05da09adabf33ed9291ed01dae639
katoyuki <0me3210g039530z@ezweb.ne.jp>
2016-09-30 15:02:05 +0000

    first commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,32 @@
+class ContactsController < ApplicationController
+    def new
+        @contact = Contact.new
+        if params[:back]
+            @contact = Contact.new(contacts_params)
+        else
+            @contact = Contact.new
+        end
+
+    end
+    
+    def confirm
+        @contact = Contact.new(contacts_params)
+        render :new if @contact.invalid?
+    end    
+    
+    def create
+        @contact = Contact.new(contacts_params)
+        if @contact.save
+        flash[:success] = 'お問い合わせありがとうございました！'
+        redirect_to root_path
+        else
+        render action: 'new'
+        end
+    end
+
+  private
+  def contacts_params
+    params.require(:contact).permit(:name, :email, :content)
+  end
+
+end
---
+++ app/helpers/contacts_helper.rb
@@ -0,0 +1,8 @@
+module ContactsHelper
+    def choose_new_or_edit
+        if action_name == 'new' || action_name == 'confirm'
+            confirm_contacts_path
+        end
+    end
+
+end
