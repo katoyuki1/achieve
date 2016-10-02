5ef6501bba54268edd6d6fa2052c6d3968a15b0c
katoyuki <0me3210g039530z@ezweb.ne.jp>
2016-10-02 05:09:21 +0000

    DIVE9

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  email = "example-#{n+1}@railstutorial.org"
+  name  = Faker::Name.name
+  password = "password"
+  User.create!(email: email,
+               name: name,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+100.times do |n|
+  Blog.create!(title: "a#{n+1}",
+               content: "a#{n+1}",
+               user_id: "#{n+1}"
+               )
+end
