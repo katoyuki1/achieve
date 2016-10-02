3b020cd7b9451bd4fb059151899d576ebdca94ee
katoyuki <0me3210g039530z@ezweb.ne.jp>
2016-10-02 09:11:28 +0000

    DIVE11 7

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -5,10 +5,15 @@ class NoticeMailer < ApplicationMailer
   #
   #   en.notice_mailer.sendmail_blog.subject
   #
+
   def sendmail_blog(blog)
     @blog = blog
 
-    mail to: "0me3210g039530z@gmail.com"
-         subject: '【Achieve】ブログが投稿されました'
+    mail(
+      to:      "0me3210g039530z@gmail.com",
+      subject: '【Achieve】ブログが投稿されました',#{contact.name}
+    ) do |format|
+      format.html
+    end
   end
 end
