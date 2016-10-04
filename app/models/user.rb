104d39d2cbc7f32b5f4ff87422a0d78bcbc0c007
katoyuki <0me3210g039530z@ezweb.ne.jp>
2016-10-04 03:22:04 +0000

    DIVE14 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :blogs
+  has_many :blogs, :dependent => :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
 
